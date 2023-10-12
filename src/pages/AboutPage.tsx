ca1b5035c775a9e663c2e93f4c37526fef89e8ec
SobinDA <SobinDA@srgroup.ru>
2023-10-12 14:31:04 +0300

    update style

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export function AboutPage() {
     return (
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At iusto minus molestiae non quibusdam quod
-            reprehenderit velit. Eius enim error harum, minus natus non obcaecati perferendis praesentium qui quisquam
-            quos repudiandae rerum sed ullam, voluptate! Ad commodi dignissimos dolor earum eum illum molestias porro
-            possimus rem! Consequuntur error minima mollitia? sd
-        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At iusto minus
+            molestiae non quibusdam quod
+            reprehenderit velit. Eius enim error harum, minus natus non obcaecati perferendis praesentium qui
+            quisquam
+            quos repudiandae rerum sed ullam, voluptate! Ad commodi dignissimos dolor earum eum illum molestias
+            porro
+            possimus rem! Consequuntur error minima mollitia? sd</p>
     )
 }
