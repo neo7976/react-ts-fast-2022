bd36bd1c2f5b216b2fd8154159ee7743c375b2d0
Dmitrii Sobin <sobinda@srgroup.ru>
2023-10-16 11:47:21 +0300

    Настройка QueryApp.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React, {FC} from "react";
 import {Route, Routes} from "react-router-dom";
 import {ProductPage} from "./pages/ProductPage";
-import {AboutPage} from "./pages/AboutPage";
 import {Navigation} from "./components/Navigation";
 import {MarvelPage} from "./pages/MarvelPage";
 import MarvelCharacterPage from "./pages/MarvelCharacterPage";
 import MarvelComicsPage from "./pages/MarvelComicsPage";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import QueryApp from "./QueryApp";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false
+        }
+    }
+})
 const App: FC = () => {
     return (
         <>
             <Navigation/>
             <Routes>
                 <Route path='/' element={<ProductPage/>}/>
-                <Route path='/about' element={<AboutPage/>}/>
+                <Route path='/about' element={
+                    <QueryClientProvider client={queryClient}>
+                        <QueryApp/> </QueryClientProvider>}/>
                 <Route path='/marvels' element={<MarvelPage/>}/>
                 <Route path='marvel/character/:id' element={<MarvelCharacterPage/>}/>
                 <Route path='marvel/comics/:id' element={<MarvelComicsPage/>}/>
