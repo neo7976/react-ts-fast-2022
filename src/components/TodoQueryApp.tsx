1510c749b74755eb1fdf199800e11242751d64ab
Dmitrii Sobin <sobinda@srgroup.ru>
2023-10-16 15:47:29 +0300

    useQueryClient

--- src/components/TodoQueryApp.tsx
+++ src/components/TodoQueryApp.tsx
@@ -1,17 +1,26 @@
 import React, {FC} from 'react';
 import Loader from "./Loader";
 import {useTodos} from "../hooks/useTodos";
+import {useQueryClient} from "@tanstack/react-query";
 
 const TodoQueryApp: FC = () => {
     const {isLoading, data} = useTodos();
 
+    //Имеется кучу свойтв, которые можно вызвать откуда угодно и обновить данные
+    const queryClient = useQueryClient();
+
 
     return (
-        <div>
+        <div className={'content'}>
+            //Для Обновления данных
+            <button className={'text-white'} onClick={() => queryClient.invalidateQueries(['todos'])}>
+                Refresh
+            </button>
             {isLoading ?
                 (<Loader/>
                 ) : data ? (data.map(todo => (
-                        <div><p>{todo.id}:{todo.title}</p></div>
+
+                        <div key={todo.id}><p>{todo.id}:{todo.title}</p></div>
                     ))
                 ) : (
                     <p>Данных нет</p>
